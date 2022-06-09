17068afd39d2673d34e63333cb34bccaca179847
elther <elther@naver.com>
2022-06-10 00:39:01 +0900

    practice : response

---
+++ src/main/java/com/elther/springmvc/basic/HelloData.java
@@ -0,0 +1,11 @@
+package com.elther.springmvc.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
